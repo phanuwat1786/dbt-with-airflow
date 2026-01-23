32f88ed05ff60e2130a7ed0994f04c5f8edc3207
phanuwat.su <phanuwat_su@inet.co.th>
2026-01-23 17:06:49 +0700

    add gold_price_hr model

---
+++ dbt/projects/process_market_price/models/marts/gold_price_hr.sql
@@ -0,0 +1,7 @@
+WITH gold as (
+    SELECT * FROM {{ ref('stg_convert_gold_timestamp') }}
+)
+
+SELECT MAX(price), currency, unit, timestamp::date + MAKE_TIME(DATE_PART('hour',timestamp)::int,0,00.0) as rounded_timestamp FROM gold
+GROUP BY currency, unit, rounded_timestamp
+ORDER BY rounded_timestamp
