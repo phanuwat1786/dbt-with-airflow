cbb1a80b63a2162373b7286f9a25fcbb4521cb24
phanuwat.su <phanuwat_su@inet.co.th>
2026-01-27 14:38:30 +0700

    feat: add gold price 1-day change model and corresponding test

---
+++ dbt/projects/process_market_price/models/marts/gold_price_1day_change.sql
@@ -0,0 +1,32 @@
+WITH market_close_time as (
+    SELECT (((NOW() AT TIME ZONE 'Asia/Bangkok')::date + MAKE_TIME(3,0,0.0)) AT TIME ZONE 'Asia/Bangkok') AT TIME ZONE 'Asia/Bangkok' as close_time
+    ),
+    gold_price_at_market_close AS (
+        SELECT price, rounded_timestamp 
+        FROM {{ ref('gold_price_hr') }} 
+        WHERE rounded_timestamp <= (SELECT close_time FROM market_close_time LIMIT 1)
+        ORDER BY rounded_timestamp DESC LIMIT 1
+    ),
+    current_gold_price AS (
+        SELECT price,rounded_timestamp FROM {{ ref('gold_price_hr') }} ORDER BY rounded_timestamp DESC LIMIT 1
+    ),
+    detail AS (
+        SELECT 
+            (
+                SELECT close_time FROM market_close_time
+            ) ,
+            (
+                SELECT price FROM gold_price_at_market_close
+            ) as closetime_price,
+            (
+                SELECT rounded_timestamp FROM gold_price_at_market_close
+            ) as close_price_timestamp,
+            (
+                SELECT price FROM current_gold_price 
+            ) as current_price ,
+            (
+                SELECT rounded_timestamp FROM current_gold_price
+            ) as current_price_timestamp 
+    )
+
+SELECT *, (current_price-closetime_price)/closetime_price * 100  AS "one-day-change" FROM detail
